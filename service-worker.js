dabddb949f16640a7fe59265f48eddce0d598064
mmorenorissi <msh8nvvn89@privaterelay.appleid.com>
2026-09-22 23:15:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mantenimiento-puentes-grua-ant-v8';
+const CACHE = 'mantenimiento-puentes-grua-ant-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
